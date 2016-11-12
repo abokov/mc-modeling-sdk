1d14ada79bd8164ad6e93fca7127b2d70e280c3d
alexey bokov <bokov@bokov.net>
2016-11-12 10:07:27 +0300

    Update swift_connector.h

--- swift/swift_connector.h
+++ swift/swift_connector.h
@@ -1,1 +1,27 @@
-// first commit
+// connector to Swift c++ SDK
+
+#ifndef __SWIFT_CONNECTOR_INC__
+#define __SWIFT_CONNECTOR_INC__
+
+#include <mc-common/mc_base_types.h>
+
+// Open Stack Swift headers...
+#include <Header.h>
+#include <Tenant.h>
+#include <Service.h>
+
+namespace McSwift {
+  
+class McSwiftConnector {
+    Swift::Service *swift_service;
+    Swift::Tenant * tenant;
+public:
+    McSwiftConnector();
+    ~McSwiftConnector();
+  
+    bool Init(/*something should be there*/void);
+    bool GetData(PositionJobData &job);
+ }
+
+
+#endif
