18c6ea7139729dbbe3c06e1e03bc7e7aa12a3126
alexey bokov <bokov@bokov.net>
2016-11-12 10:41:44 +0300

    Update mc-modeling-cmd.cpp
    Add horizon and iters num to command line

--- mc-modeling-cmd.cpp
+++ mc-modeling-cmd.cpp
@@ -11,7 +11,7 @@
 void usage(void);
 
 int main( int argc, const char* argv[] ) {
-    if (argc != 3 ) {
+    if (argc != 4) {
         usage();
         return 1;
     }
@@ -30,12 +30,27 @@ int main( int argc, const char* argv[] ) {
 		std::cout<<" error: Wrong build of binary - AZURE_BUILD should be defined on build time!"<<std::endl;
 #endif		
 	} else if (argv[2] == "--local") {
-//		get_azure_data(PositionJobData,argv[3],argv[4])
+		get_azure_data(jobData,argv[3],argv[4])
 		// get data from CSV
 	} else {
 		usage();
 		return 1;
 	}
+	if (argv[3] == "--iterations") {
+		int num = std::stoi( argv[4] );
+		jobData.SetIters(num);
+	} else {
+		usage();
+		return 1;
+	};
+
+	if (argv[5] == "--horizon") {
+		int num = std::stoi( argv[6] );
+		jobData.SetHorizon(num);
+	} else {
+		usage();
+		return 1;
+	};
 
     // put data into processing class
 	PositionProcessor processor(jobData);
@@ -54,10 +69,11 @@ int main( int argc, const char* argv[] ) {
 
 void usage(void) {
     std::cout<<
-	"Usage: mc-modeling-cmd config_file.xml [-azure storage_account blob_name]|[-local input_file]\n"
+	"Usage: mc-modeling-cmd config_file.xml [-azure storage_account blob_name]|[-local input_file] -iterations number\n"
 	" Azure mode: upload historical data from azure storage account, output will be in stdout\n"
-	"   example:  mc-modeling-cmd config_file.xml -azure TestAccount  NASDAQ_DATA\n"
+	" Please use params in order from below, change order not supported yet\n"
+	"   example:  mc-modeling-cmd config_file.xml -azure TestAccount  NASDAQ_DATA  -iterations 10000000 -horizon 20\n"
 	" Local (standalone) mode: upload date from local file ( run locally ), output will be in stdout\n"
-	"   example:  mc-modeling-cmd config_file.xml -local /home/user/my_data.csv"
+	"   example:  mc-modeling-cmd config_file.xml -local /home/user/my_data.csv -iterations 10000000 -horizon 10" 
 	<<std::endl;
 }
