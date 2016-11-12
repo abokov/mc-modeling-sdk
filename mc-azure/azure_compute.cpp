9a0bc128bb76f83bf13d027386c110bdf759b2e8
alexey bokov <bokov@bokov.net>
2016-11-12 10:31:07 +0300

    Create azure_compute.cpp

---
+++ mc-azure/azure_compute.cpp
@@ -0,0 +1,22 @@
+#include "azure_compute.h"
+
+#ifdef 0
+
+// to do - impelement thatone
+class AzureCompute {
+
+public:
+    AzureCompute();
+    ~AzureCompute();
+    
+    bool CreateAzureVM(AzureVM&vm );
+};
+
+#endif
+
+    Contact GitHub API Training Shop Blog About 
+
+    © 2016 GitHub, Inc. Terms Privacy Security Status Help 
+
+
+#endif
